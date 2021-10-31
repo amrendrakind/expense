d6dc9bb24727c52236d7653470e358f3ea4c425c
amrendrakind <vision2010@gmail.com>
2021-11-01 00:09:39 +0530

    Expense Tracker

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NewExpense from './components/NewExpense/NewExpense';
 const DUMMY_EXPENSES = [
   { id:'e1', title: "Toilet Paper", amount: 194.67, date: new Date(2021, 9, 1) },
   { id:'e2', title: "New TV", amount: 799.67, date: new Date(2021, 8, 1) },
-  { id:'e3', title: "New Desk (Wooden)", amount: 450.05, date: new Date(2021, 6, 1) },
+  { id:'e3', title: "New Desk (Wooden)", amount: 450.05, date: new Date(2020, 6, 1) },
   { id:'e4', title: "Car Insurance", amount: 294.67, date: new Date(2020, 9, 1) },
 ]
 
