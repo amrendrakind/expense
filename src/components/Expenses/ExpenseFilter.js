39431d7173bca290a7c0b772c7d2e61af33e2b24
amrendrakind <vision2010@gmail.com>
2021-11-01 00:11:40 +0530

    Expense Tracker

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -10,10 +10,10 @@ const ExpenseFilter = (props)=>{
         <div className = "expense-filter__control">
             <label>Filter By Year</label>
             <select value={props.selected} onChange={dropdownChangeHandler}>
-                <option value='2022'>2022</option>
                 <option value='2023'>2023</option>
-                <option value='2020'>2020</option>
+                <option value='2022'>2022</option>
                 <option value='2021'>2021</option>
+                <option value='2020'>2020</option>
             </select>
         </div>
     </div>
