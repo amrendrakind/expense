0cf01cb552447943a047a7d81385e5bc57397d95
amrendrakind <vision2010@gmail.com>
2022-01-27 14:32:21 +0530

    ExpenseItem.css added data using props added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import './App.css';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Care Insurance",
+      amount: 294.67,
+      date: new Date(2022, 1, 27),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 94.67,
+      date: new Date(2021, 3, 25),
+    },
+    {
+      id: "e3",
+      title: "Toilet Paper",
+      amount: 29.67,
+      date: new Date(2021, 12, 2),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 300.67,
+      date: new Date(2020, 1, 12),
+    },
+  ];
   return (
     <div>
       <h1>Lets get Started!</h1>
-      <p>This is also visible on the Screen</p>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
+
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <h2>{props.title}</h2>
+      <div className="=expense-item__description">
+        <div className="expense-item__price">₹{props.amount}</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
