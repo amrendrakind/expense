755bbb8f3cf0f56352fad87bf4bc0f571cdeed3e
amrendrakind <vision2010@gmail.com>
2022-01-30 11:56:09 +0530

    Dynamically adding data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,25 +14,25 @@ const Expenses = (props) => {
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+  let expensesContent = <p>No Expenses Found</p>
 
+  if(filteredExpenses.length>0){
+    expensesContent =  filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.length === 0 ? (
-        <p>No Expenses Found</p>
-      ) : (
-        filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))
-      )}
+      {expensesContent}
     </Card>
   );
 };
